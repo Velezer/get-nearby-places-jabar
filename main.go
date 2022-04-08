02701b2d35914e2c679e8e2b5f94893106501720
Velezer <asvelezer@gmail.com>
2022-04-08 21:24:36 +0700

    fix: no need to cache in the beginning

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"jabar-nearby-places/routes"
 	"jabar-nearby-places/seed"
-	"jabar-nearby-places/services"
 )
 
 func main() {
-	go seed.Begin()                     // seed if never been seeded
-	go services.PlaceService.FindAll(0) // will cache findall result for increasing search performance
+	go seed.Begin() // seed if never been seeded
 
 	r := routes.SetupRouter()
 	r.Run()
