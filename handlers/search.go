501265dccaef3d04fd2778234136be54a47acd41
Velezer <asvelezer@gmail.com>
2022-04-09 00:51:23 +0700

    feat: validation for lat and lon

--- handlers/search.go
+++ handlers/search.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"errors"
+	"jabar-nearby-places/httperror"
 	"jabar-nearby-places/services"
 	"jabar-nearby-places/utils"
 	"net/http"
@@ -9,10 +11,14 @@ import (
 )
 
 func Search(c *gin.Context) {
-	var lat float64 = utils.ParseFloat64(c.Query("latitude"), -999)
-	var lon float64 = utils.ParseFloat64(c.Query("longitude"), -999)
+	var lat float64 = utils.ParseFloat64(c.Query("latitude"), -9999)
+	var lon float64 = utils.ParseFloat64(c.Query("longitude"), -9999)
+	if lat == -9999 || lon == -9999 {
+		c.Error(errors.New("latitude and longitude is required")).SetMeta(httperror.NewMeta(http.StatusBadRequest))
+		return
+	}
 	var categoryId uint = utils.ParseUint(c.Query("category_id"), 0)
-	
+
 	founds, err := services.PlaceService.FindAll(categoryId)
 	if err != nil {
 		c.Error(err)
